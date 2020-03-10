d557b02c2d6732b8388091c749ae7a5da5855a68
Gregg Sperling <gsperling@my.wctc.edu>
2020-03-09 20:58:12 -0500

    Gregg Sperling v1.0

---
+++ RaceTrack/Cars/ToyotaCorolla.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class ToyotaCorolla : RaceCar
+    {
+        public ToyotaCorolla()
+        {
+            Name = "Toyota Corolla";
+            TopSpeed = 110; // It's the Model S!
+        }
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} fires up immediately, with its engine purring like a kitten.");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} comes to an exceedingly quick stop, almost as if on a dime!");
+            base.Brake();
+        }
+
+        public override void StopEngine()
+        {
+            Console.WriteLine($"The {Name}'s engine is commanded to halt, and halt it does.");
+        }
+    }
+}
---
+++ RaceTrack/Drivers/Gregg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Gregg : Driver
+    {
+        public Gregg(RaceCar car) : base(car)
+        {
+            Name = "Gregg";
+            SkillLevel = 15;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+    }
+}
